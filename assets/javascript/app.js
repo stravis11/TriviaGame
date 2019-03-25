befe966a2b2b118deba642ec0afa81db9f54097d
Steve Travis <stevetravis11@gmail.com>
2019-03-25 14:26:56 -0400

    wip: Timer function created.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
 
     //Global variables
+    var correct = 0;
+    var incorrect = 0;
+    var questionTime = 30;
+    var timeID;
+
+
+    //Question and answer array
     var questions = [{
         question: "What color is the sky?",
         choices: ["Red","Blue","Green","Yellow"],
@@ -19,9 +26,28 @@ $(document).ready(function() {
 
     //Start game event listener
     $("#startBtn").click(function() {
-        alert("Game started");
+        // alert("Game started");
+        $("startBtn").hide();
+        timer();
+        timeID = setInterval(timer,1000);
+        console.log(timeID);
+        startquiz();
+
     });
 
 
+    //Timer function
+    function timer() {
+        if (questionTime !== 0) {
+            questionTime--
+            var $time = $("<h2>Time Remaining 00:" +questionTime+ " </h2>");
+            $("#time").html($time);
+        }
+    }
+
+    //Starting the trivia questions
+        function startquiz () {
+          $("#questions").html("Game started!");  
+        }
 
 });
